d59522b8ac71b8e281b75e12561fe0671d4c7c57
iain@lavander <iainvisa@gmail.com>
2025-12-15 23:57:28 +0000

    Hello World

---
+++ hello.cpp
@@ -0,0 +1,35 @@
+// Necessary header files for input output functions
+#include <iostream>
+#include <string>
+using namespace std;
+
+// main() function: where the execution of
+// C++ program begins
+
+
+
+int test() {
+	char s_in;
+	int i_in;
+	cin >> s_in;
+	i_in = s_in;
+
+
+	return 0;
+}
+
+
+
+
+int main() {
+  
+    // This statement prints "Hello World"
+    cout << "Hello World\n";
+    int o;
+    o = test();
+    cout << o;
+    cout << ">>"+to_string(o) + "\n";
+
+    return 0;
+}
+
